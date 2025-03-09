f124304ebad2a578fe9dd3424a0976184de0e07f
李启辉 <522903183@qq.com>
2025-03-09 15:18:55 +0800

    打包配置修改； 获取git内容信息；

--- send_msg.py
+++ send_msg.py
@@ -8,19 +8,23 @@ secret = 'SEC99ed0136be5b39fe17ccc251ca6d7edca0f168afbefb78fcedcd76a6185f19f5'  
 # # 初始化机器人小丁
 xiaoding = DingtalkChatbot(webhook, secret=secret)
 # 获取最新的提交信息
+# commit_info = subprocess.check_output(
+#     ['git', 'log', '-1', '--pretty=format:%H by %an on %ad%n%s', '--date=short'],
+#     encoding='utf-8'
+# ).strip()
+# 获取最新的提交信息，仅保留提交信息部分
 commit_info = subprocess.check_output(
-    ['git', 'log', '-1', '--pretty=format:%H by %an on %ad%n%s', '--date=short'],
+    ['git', 'log', '-1', '--pretty=format:%s'],
     encoding='utf-8'
 ).strip()
-
 # 发送 Markdown 消息，包含提交信息
 xiaoding.send_markdown(
     title='Auki iOS打包',
-    text=f'[Auki_iOS线上包_1.0.0 👉点击下载](https://www.pgyer.com/idoWP9yR)\n\n'
+    text=f'[Auki_iOS线上包_v1.0.0 👉点击下载](https://www.pgyer.com/idoWP9yR)\n\n'
          '#### 安装密码：123456 \n\n'
          '> ![下载](https://www.pgyer.com/app/qrcode/idoWP9yR?pixsize=258)\n\n'
          '>  \n\n'
-         f'### 🆕 最新提交信息：\n'
+         f'### 🆕 提测试内容：\n'
          f'> {commit_info}\n\n',
     at_mobiles=['13104888483']
 )
